b80f0c1a28b56af7f177f9e715e24b740bcf8003
eudavidcesar <contactdaviddecarvalho@gmail.com>
2020-11-08 14:31:56 -0300

    Arquivos deletados

--- app.js
+++
@@ -1,4 +0,0 @@
-import { pessoa } from './module.js'
-
-const pes = pessoa.nome
-console.log(pes)
--- module.js
+++
@@ -1,7 +0,0 @@
-export { pessoa }
-
-var pessoa = {
-    'nome': 'Vitória',
-    'idade': 18,
-    'burrice': '100%'
-}
